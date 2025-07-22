693cc2fa607703f00106a2ab778837a6a3c8ed68
Utkarsh Upadhyay <146931131+Utkarsh-130@users.noreply.github.com>
2025-07-23 02:33:30 +0530

    Implementing postgre

--- src/main/java/com/fitness/userservice/model/User.java
+++ src/main/java/com/fitness/userservice/model/User.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -19,7 +20,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private Long id;
+    private UUID id;
 
     @Column(nullable = false, unique = true)
     private String email;
