b16b95a47c0853c065f58fce33352e85eae1f2f5
Utkarsh Upadhyay <146931131+Utkarsh-130@users.noreply.github.com>
2025-08-04 23:57:58 +0530

    Add ActivityController and DTOs, refactor model package
    Introduces ActivityController with a trackActivity endpoint and adds ActivityRequest and ActivityResponse DTOs for activity tracking. Refactors model classes into a new package and updates Activity model with MongoDB annotations. Removes unused EmbeddingModelConfig.

---
+++ activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -0,0 +1,20 @@
+package com.fitness.activityservice.controller;
+
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/activities")
+public class ActivityController {
+    @PostMapping
+    public ResponseEntity<ActivityResponse> trackActivity(ActivityRequest request) {
+
+
+        return ResponseEntity.ok(activityService.trackActivity(request));
+    }
+
+}
--- activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
+++ activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
@@ -5,14 +5,20 @@ import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDateTime;
 import java.util.Map;
-
+@Document(collection = "activities")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Activity {
     @Id
     private String id;
